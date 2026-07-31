9915dd3bd4ab9a2428f9c089996feb2c7e393bdd
root <root@localhost.localdomain>
2026-07-31 03:18:29 -0400

    feat: Add User

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
+    <div>User</div>
   </main>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,4 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-
-// Test Debug Info
-console.log('-------test-----')
 createApp(App).mount('#app')
